1a2541026521c98f92bd0f9691044ee46d3ec061
RochaDanielPrado <daniel.rocha@rocce.com.br>
2022-10-04 16:17:37 -0300

    zerar

--- screens/loginscreen.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
-
-# from kivy.uix.screenmanager import Screen
-from kivymd.app import MDApp
-from kivymd.uix.screen import MDScreen
-
-class LoginScreen(MDScreen):
-    pass
--- screens/screenmanager.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
-
-from kivy.uix.screenmanager import ScreenManager
-
-sm = ScreenManager()
-
-class MainScreenManager(ScreenManager):
-    pass
